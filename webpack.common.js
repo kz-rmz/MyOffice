61783d46bd8cdba912e22f492a606581ab57f1f9
kz-rmz <magazumov@gmail.com>
2020-01-06 23:45:40 +0600

    Added new pages, scss file devided on components

--- webpack.common.js
+++ webpack.common.js
@@ -35,6 +35,15 @@ module.exports = {
                 test: /\.html$/,
                 use: ['html-loader']
             }, {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'fonts'
+                    }
+                }]
+            }, {
                 test: /\.(png|jpg|svg|gif)$/i,
                 use: {
                     loader: 'file-loader',
