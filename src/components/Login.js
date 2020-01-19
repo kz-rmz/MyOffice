abd25dfd84b7292c98ed7a0dec7ea9cd85a32637
kz-rmz <magazumov@gmail.com>
2020-01-20 00:12:26 +0600

    docs folder updated

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,2 @@
-import React, { useState } from "react";
-import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
-import "./Login.css";
+
 
--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Request from '../components/Request.js';
+import Profile from '../components/Profile.js';
 import '../sass/style.scss';
-import './index.js'
 import newRequest from './index.js';
 
 newRequest();
 
 ReactDOM.render(<Request />, document.getElementById('root'));
+ReactDOM.render(<Profile />, document.getElementById('profile'));
